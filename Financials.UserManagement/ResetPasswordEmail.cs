444d641cc2379c3391088cd1ed10d114c735f4e1
dan <ofbadmin@ofbbutte.com>
2019-12-20 23:43:06 -0700

    Add methods to user

---
+++ Financials.UserManagement/ResetPasswordEmail.cs
@@ -0,0 +1,24 @@
+﻿using Financials.Common.Email;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financials.UserManagement
+{
+    class ResetPasswordEmail : EmailMessage
+    {
+        public override string Template { get; set; } = @"
+            <p>Please reset your password by clicking the link below.</p>
+            <p><a href='VerifyUrl'>{{Url}}</a></p>
+        ";
+
+        public override string Subject { get; set; } = "Verify Email";
+
+        public string Url { get; set; }
+
+        public ResetPasswordEmail(string user, string code)
+        {
+            Url = $"https://localhost:4200?code={code}&user={user}";
+        }
+    }
+}
--- Financials.UserManagement/User.cs
+++ Financials.UserManagement/User.cs
@@ -9,6 +9,7 @@ namespace Financials.UserManagement
         public ObjectId Id { get; private set; }
         public string Username { get; private set; }
         public Email Email { get; private set; }
+        public bool EmailVerified { get { return Email.IsVerified(); } }
         public PasswordHashed PasswordHashed { get; private set; }
         public ResetPasswordCode ResetPasswordCode { get; private set; }
         public Profile Profile { get; private set; }
